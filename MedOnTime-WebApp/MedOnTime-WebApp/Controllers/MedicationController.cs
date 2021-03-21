1fd34276a34b51a5c4f093d61fb635b49b037b86
kenye <kenye@KENDRICK>
2021-03-21 18:54:37 -0400

    Added Show List, Details, Edit and Delete operations for Medication

--- MedOnTime-WebApp/MedOnTime-WebApp/Controllers/MedicationController.cs
+++ MedOnTime-WebApp/MedOnTime-WebApp/Controllers/MedicationController.cs
@@ -1,7 +1,9 @@
 ﻿using MedOnTime_WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MedOnTime_WebApp.Controllers
 {
@@ -34,16 +36,16 @@ namespace MedOnTime_WebApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult MedicationForm(Medication formResponse)
         {
-            if (ModelState.IsValid) 
-            { 
+            if (ModelState.IsValid)
+            {
                 System.Diagnostics.Debug.WriteLine(formResponse.MedicationName + ", " + formResponse.MethodOfTaking + ", " + formResponse.Dosage + ", " + formResponse.Quantity + ", " + formResponse.MedicationType);
                 try
                 {
                     List<Medication> existingMeds = _medicationCollection.AsQueryable<Medication>().ToList();
                     if (existingMeds.Count == 0)
-                        formResponse._id = 1;
+                        formResponse.MedID = 1;
                     else
-                        formResponse._id = existingMeds[existingMeds.Count - 1]._id + 1;
+                        formResponse.MedID = existingMeds[existingMeds.Count - 1].MedID + 1;
 
                     _medicationCollection.InsertOne(formResponse);
                     return RedirectToAction("Index", "Home");
@@ -51,6 +53,58 @@ namespace MedOnTime_WebApp.Controllers
                 catch { return View(formResponse); }
             }
             return View(formResponse);
+        }
+
+        [HttpGet]
+        public IActionResult MedicationEdit(int medID)
+        {
+            var medication = _medicationCollection.AsQueryable<Medication>().SingleOrDefault(x => x.MedID == medID);
+            return View(medication);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult MedicationEdit(string objID, Medication formResponse)
+        {
+            try
+            {
+                var filter = Builders<Medication>.Filter.Eq("_id", ObjectId.Parse(objID));
+                var update = Builders<Medication>.Update
+                    .Set("MedicationName", formResponse.MedicationName)
+                    .Set("MethodOfTaking", formResponse.MethodOfTaking)
+                    .Set("Dosage", formResponse.Dosage)
+                    .Set("MedicationType", formResponse.MedicationType)
+                    .Set("Quantity", formResponse.Quantity);
+                var result = _medicationCollection.UpdateOne(filter, update);
+                return RedirectToAction("MedicationList");
+            }
+            catch { return View(formResponse); }
+        }
+
+        [HttpGet]
+        public IActionResult MedicationDetails(int medID)
+        {
+            var medication = _medicationCollection.AsQueryable<Medication>().SingleOrDefault(x => x.MedID == medID);
+            return View(medication);
+        }
+
+        [HttpGet]
+        public IActionResult MedicationDelete(int medID)
+        {
+            var medication = _medicationCollection.AsQueryable<Medication>().SingleOrDefault(x => x.MedID == medID);
+            return View(medication);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult MedicationDelete(string objID, Medication formResponse)
+        {
+            try
+            {
+                _medicationCollection.DeleteOne(Builders<Medication>.Filter.Eq("_id", ObjectId.Parse(objID)));
+                return RedirectToAction("MedicationList");
+            }
+            catch { return View(formResponse); }
         }
     }
 }
--- MedOnTime-WebApp/MedOnTime-WebApp/Models/Medication.cs
+++ MedOnTime-WebApp/MedOnTime-WebApp/Models/Medication.cs
@@ -1,29 +1,47 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace MedOnTime_WebApp.Models
 {
     public class Medication
     {
+        [BsonId]
+        public ObjectId Id { get; set; }
+
         public int? PrescriptionID { get; set; }
-        public int _id { get; set; }
+
+        [BsonElement("MedID")]
+        public int MedID { get; set; }
+
         [Required(ErrorMessage = "Please enter medication name")]
+        [BsonElement("MedicationName")]
         public string MedicationName { get; set; }
+
         [Required(ErrorMessage = "Please enter the method of taking")]
+        [BsonElement("MethodOfTaking")]
         public string MethodOfTaking { get; set; }
+
         public int? MedicationImage { get; set; }
+
         [Required(ErrorMessage = "Please enter the dosage")]
+        [BsonElement("Dosage")]
         public string Dosage { get; set; }
+
         [Required(ErrorMessage = "Please select the type of taking")]
+        [BsonElement("MedicationType")]
         public string MedicationType { get; set; }
+
         [Required(ErrorMessage = "Please select the quantity")]
+        [BsonElement("Quantity")]
         public int? Quantity { get; set; }
 
         public Medication() { }
 
-        public Medication(int? prescriptionID, int _id, string medicationName, string methodOfTaking, int? medImage, string dosage, string medicationType, int quanitity)
+        public Medication(int? prescriptionID, int medID, string medicationName, string methodOfTaking, int? medImage, string dosage, string medicationType, int quanitity)
         {
             this.PrescriptionID = prescriptionID;
-            this._id = _id;
+            this.MedID = medID;
             this.MedicationName = medicationName;
             this.MethodOfTaking = methodOfTaking;
             this.MedicationImage = medImage;
